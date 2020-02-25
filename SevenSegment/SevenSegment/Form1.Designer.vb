057980c852dd8b69dda34ff8eb622459cdbaf01f
Colyn Mariz Atencio <53134108+colynero@users.noreply.github.com>
2020-02-25 19:11:38 +0800

    Add files via upload

---
+++ SevenSegment/SevenSegment/Form1.Designer.vb
@@ -0,0 +1,379 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.FlowLayoutPanel1 = New System.Windows.Forms.FlowLayoutPanel()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.TextBox6 = New System.Windows.Forms.TextBox()
+        Me.TextBox7 = New System.Windows.Forms.TextBox()
+        Me.TextBox8 = New System.Windows.Forms.TextBox()
+        Me.TextBox9 = New System.Windows.Forms.TextBox()
+        Me.TextBox10 = New System.Windows.Forms.TextBox()
+        Me.TextBox11 = New System.Windows.Forms.TextBox()
+        Me.TextBox12 = New System.Windows.Forms.TextBox()
+        Me.TextBox13 = New System.Windows.Forms.TextBox()
+        Me.TextBox14 = New System.Windows.Forms.TextBox()
+        Me.TextBox15 = New System.Windows.Forms.TextBox()
+        Me.TextBox16 = New System.Windows.Forms.TextBox()
+        Me.TextBox17 = New System.Windows.Forms.TextBox()
+        Me.TextBox18 = New System.Windows.Forms.TextBox()
+        Me.TextBox19 = New System.Windows.Forms.TextBox()
+        Me.TextBox20 = New System.Windows.Forms.TextBox()
+        Me.TextBox21 = New System.Windows.Forms.TextBox()
+        Me.TextBox22 = New System.Windows.Forms.TextBox()
+        Me.TextBox23 = New System.Windows.Forms.TextBox()
+        Me.TextBox24 = New System.Windows.Forms.TextBox()
+        Me.TextBox25 = New System.Windows.Forms.TextBox()
+        Me.FlowLayoutPanel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'FlowLayoutPanel1
+        '
+        Me.FlowLayoutPanel1.BackColor = System.Drawing.Color.MediumAquamarine
+        Me.FlowLayoutPanel1.Controls.Add(Me.TextBox1)
+        Me.FlowLayoutPanel1.Controls.Add(Me.TextBox2)
+        Me.FlowLayoutPanel1.Controls.Add(Me.TextBox3)
+        Me.FlowLayoutPanel1.Controls.Add(Me.TextBox4)
+        Me.FlowLayoutPanel1.Controls.Add(Me.TextBox5)
+        Me.FlowLayoutPanel1.Controls.Add(Me.TextBox6)
+        Me.FlowLayoutPanel1.Controls.Add(Me.TextBox7)
+        Me.FlowLayoutPanel1.Controls.Add(Me.TextBox8)
+        Me.FlowLayoutPanel1.Controls.Add(Me.TextBox9)
+        Me.FlowLayoutPanel1.Controls.Add(Me.TextBox10)
+        Me.FlowLayoutPanel1.Controls.Add(Me.TextBox11)
+        Me.FlowLayoutPanel1.Controls.Add(Me.TextBox12)
+        Me.FlowLayoutPanel1.Controls.Add(Me.TextBox13)
+        Me.FlowLayoutPanel1.Controls.Add(Me.TextBox14)
+        Me.FlowLayoutPanel1.Controls.Add(Me.TextBox15)
+        Me.FlowLayoutPanel1.Controls.Add(Me.TextBox16)
+        Me.FlowLayoutPanel1.Controls.Add(Me.TextBox17)
+        Me.FlowLayoutPanel1.Controls.Add(Me.TextBox18)
+        Me.FlowLayoutPanel1.Controls.Add(Me.TextBox19)
+        Me.FlowLayoutPanel1.Controls.Add(Me.TextBox20)
+        Me.FlowLayoutPanel1.Controls.Add(Me.TextBox21)
+        Me.FlowLayoutPanel1.Controls.Add(Me.TextBox22)
+        Me.FlowLayoutPanel1.Controls.Add(Me.TextBox23)
+        Me.FlowLayoutPanel1.Controls.Add(Me.TextBox24)
+        Me.FlowLayoutPanel1.Controls.Add(Me.TextBox25)
+        Me.FlowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.FlowLayoutPanel1.Location = New System.Drawing.Point(0, 0)
+        Me.FlowLayoutPanel1.Name = "FlowLayoutPanel1"
+        Me.FlowLayoutPanel1.Size = New System.Drawing.Size(406, 405)
+        Me.FlowLayoutPanel1.TabIndex = 0
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BackColor = System.Drawing.Color.White
+        Me.TextBox1.Enabled = False
+        Me.TextBox1.Location = New System.Drawing.Point(3, 3)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(75, 75)
+        Me.TextBox1.TabIndex = 0
+        '
+        'TextBox2
+        '
+        Me.TextBox2.BackColor = System.Drawing.Color.White
+        Me.TextBox2.Enabled = False
+        Me.TextBox2.Location = New System.Drawing.Point(84, 3)
+        Me.TextBox2.Multiline = True
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(75, 75)
+        Me.TextBox2.TabIndex = 1
+        '
+        'TextBox3
+        '
+        Me.TextBox3.BackColor = System.Drawing.Color.White
+        Me.TextBox3.Enabled = False
+        Me.TextBox3.Location = New System.Drawing.Point(165, 3)
+        Me.TextBox3.Multiline = True
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(75, 75)
+        Me.TextBox3.TabIndex = 2
+        '
+        'TextBox4
+        '
+        Me.TextBox4.BackColor = System.Drawing.Color.White
+        Me.TextBox4.Enabled = False
+        Me.TextBox4.Location = New System.Drawing.Point(246, 3)
+        Me.TextBox4.Multiline = True
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(75, 75)
+        Me.TextBox4.TabIndex = 3
+        '
+        'TextBox5
+        '
+        Me.TextBox5.BackColor = System.Drawing.Color.White
+        Me.TextBox5.Enabled = False
+        Me.TextBox5.Location = New System.Drawing.Point(327, 3)
+        Me.TextBox5.Multiline = True
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(75, 75)
+        Me.TextBox5.TabIndex = 4
+        '
+        'TextBox6
+        '
+        Me.TextBox6.BackColor = System.Drawing.Color.White
+        Me.TextBox6.Enabled = False
+        Me.TextBox6.Location = New System.Drawing.Point(3, 84)
+        Me.TextBox6.Multiline = True
+        Me.TextBox6.Name = "TextBox6"
+        Me.TextBox6.Size = New System.Drawing.Size(75, 75)
+        Me.TextBox6.TabIndex = 5
+        '
+        'TextBox7
+        '
+        Me.TextBox7.BackColor = System.Drawing.Color.White
+        Me.TextBox7.Enabled = False
+        Me.TextBox7.Location = New System.Drawing.Point(84, 84)
+        Me.TextBox7.Multiline = True
+        Me.TextBox7.Name = "TextBox7"
+        Me.TextBox7.Size = New System.Drawing.Size(75, 75)
+        Me.TextBox7.TabIndex = 6
+        '
+        'TextBox8
+        '
+        Me.TextBox8.BackColor = System.Drawing.Color.White
+        Me.TextBox8.Enabled = False
+        Me.TextBox8.Location = New System.Drawing.Point(165, 84)
+        Me.TextBox8.Multiline = True
+        Me.TextBox8.Name = "TextBox8"
+        Me.TextBox8.Size = New System.Drawing.Size(75, 75)
+        Me.TextBox8.TabIndex = 7
+        '
+        'TextBox9
+        '
+        Me.TextBox9.BackColor = System.Drawing.Color.White
+        Me.TextBox9.Enabled = False
+        Me.TextBox9.Location = New System.Drawing.Point(246, 84)
+        Me.TextBox9.Multiline = True
+        Me.TextBox9.Name = "TextBox9"
+        Me.TextBox9.Size = New System.Drawing.Size(75, 75)
+        Me.TextBox9.TabIndex = 8
+        '
+        'TextBox10
+        '
+        Me.TextBox10.BackColor = System.Drawing.Color.White
+        Me.TextBox10.Enabled = False
+        Me.TextBox10.Location = New System.Drawing.Point(327, 84)
+        Me.TextBox10.Multiline = True
+        Me.TextBox10.Name = "TextBox10"
+        Me.TextBox10.Size = New System.Drawing.Size(75, 75)
+        Me.TextBox10.TabIndex = 9
+        '
+        'TextBox11
+        '
+        Me.TextBox11.BackColor = System.Drawing.Color.White
+        Me.TextBox11.Enabled = False
+        Me.TextBox11.Location = New System.Drawing.Point(3, 165)
+        Me.TextBox11.Multiline = True
+        Me.TextBox11.Name = "TextBox11"
+        Me.TextBox11.Size = New System.Drawing.Size(75, 75)
+        Me.TextBox11.TabIndex = 10
+        '
+        'TextBox12
+        '
+        Me.TextBox12.BackColor = System.Drawing.Color.White
+        Me.TextBox12.Enabled = False
+        Me.TextBox12.Location = New System.Drawing.Point(84, 165)
+        Me.TextBox12.Multiline = True
+        Me.TextBox12.Name = "TextBox12"
+        Me.TextBox12.Size = New System.Drawing.Size(75, 75)
+        Me.TextBox12.TabIndex = 11
+        '
+        'TextBox13
+        '
+        Me.TextBox13.BackColor = System.Drawing.Color.White
+        Me.TextBox13.Enabled = False
+        Me.TextBox13.Location = New System.Drawing.Point(165, 165)
+        Me.TextBox13.Multiline = True
+        Me.TextBox13.Name = "TextBox13"
+        Me.TextBox13.Size = New System.Drawing.Size(75, 75)
+        Me.TextBox13.TabIndex = 12
+        '
+        'TextBox14
+        '
+        Me.TextBox14.BackColor = System.Drawing.Color.White
+        Me.TextBox14.Enabled = False
+        Me.TextBox14.Location = New System.Drawing.Point(246, 165)
+        Me.TextBox14.Multiline = True
+        Me.TextBox14.Name = "TextBox14"
+        Me.TextBox14.Size = New System.Drawing.Size(75, 75)
+        Me.TextBox14.TabIndex = 13
+        '
+        'TextBox15
+        '
+        Me.TextBox15.BackColor = System.Drawing.Color.White
+        Me.TextBox15.Enabled = False
+        Me.TextBox15.Location = New System.Drawing.Point(327, 165)
+        Me.TextBox15.Multiline = True
+        Me.TextBox15.Name = "TextBox15"
+        Me.TextBox15.Size = New System.Drawing.Size(75, 75)
+        Me.TextBox15.TabIndex = 14
+        '
+        'TextBox16
+        '
+        Me.TextBox16.BackColor = System.Drawing.Color.White
+        Me.TextBox16.Enabled = False
+        Me.TextBox16.Location = New System.Drawing.Point(3, 246)
+        Me.TextBox16.Multiline = True
+        Me.TextBox16.Name = "TextBox16"
+        Me.TextBox16.Size = New System.Drawing.Size(75, 75)
+        Me.TextBox16.TabIndex = 15
+        '
+        'TextBox17
+        '
+        Me.TextBox17.BackColor = System.Drawing.Color.White
+        Me.TextBox17.Enabled = False
+        Me.TextBox17.Location = New System.Drawing.Point(84, 246)
+        Me.TextBox17.Multiline = True
+        Me.TextBox17.Name = "TextBox17"
+        Me.TextBox17.Size = New System.Drawing.Size(75, 75)
+        Me.TextBox17.TabIndex = 16
+        '
+        'TextBox18
+        '
+        Me.TextBox18.BackColor = System.Drawing.Color.White
+        Me.TextBox18.Enabled = False
+        Me.TextBox18.Location = New System.Drawing.Point(165, 246)
+        Me.TextBox18.Multiline = True
+        Me.TextBox18.Name = "TextBox18"
+        Me.TextBox18.Size = New System.Drawing.Size(75, 75)
+        Me.TextBox18.TabIndex = 17
+        '
+        'TextBox19
+        '
+        Me.TextBox19.BackColor = System.Drawing.Color.White
+        Me.TextBox19.Enabled = False
+        Me.TextBox19.Location = New System.Drawing.Point(246, 246)
+        Me.TextBox19.Multiline = True
+        Me.TextBox19.Name = "TextBox19"
+        Me.TextBox19.Size = New System.Drawing.Size(75, 75)
+        Me.TextBox19.TabIndex = 18
+        '
+        'TextBox20
+        '
+        Me.TextBox20.BackColor = System.Drawing.Color.White
+        Me.TextBox20.Enabled = False
+        Me.TextBox20.Location = New System.Drawing.Point(327, 246)
+        Me.TextBox20.Multiline = True
+        Me.TextBox20.Name = "TextBox20"
+        Me.TextBox20.Size = New System.Drawing.Size(75, 75)
+        Me.TextBox20.TabIndex = 19
+        '
+        'TextBox21
+        '
+        Me.TextBox21.BackColor = System.Drawing.Color.White
+        Me.TextBox21.Enabled = False
+        Me.TextBox21.Location = New System.Drawing.Point(3, 327)
+        Me.TextBox21.Multiline = True
+        Me.TextBox21.Name = "TextBox21"
+        Me.TextBox21.Size = New System.Drawing.Size(75, 75)
+        Me.TextBox21.TabIndex = 20
+        '
+        'TextBox22
+        '
+        Me.TextBox22.BackColor = System.Drawing.Color.White
+        Me.TextBox22.Enabled = False
+        Me.TextBox22.Location = New System.Drawing.Point(84, 327)
+        Me.TextBox22.Multiline = True
+        Me.TextBox22.Name = "TextBox22"
+        Me.TextBox22.Size = New System.Drawing.Size(75, 75)
+        Me.TextBox22.TabIndex = 21
+        '
+        'TextBox23
+        '
+        Me.TextBox23.BackColor = System.Drawing.Color.White
+        Me.TextBox23.Enabled = False
+        Me.TextBox23.Location = New System.Drawing.Point(165, 327)
+        Me.TextBox23.Multiline = True
+        Me.TextBox23.Name = "TextBox23"
+        Me.TextBox23.Size = New System.Drawing.Size(75, 75)
+        Me.TextBox23.TabIndex = 22
+        '
+        'TextBox24
+        '
+        Me.TextBox24.BackColor = System.Drawing.Color.White
+        Me.TextBox24.Enabled = False
+        Me.TextBox24.Location = New System.Drawing.Point(246, 327)
+        Me.TextBox24.Multiline = True
+        Me.TextBox24.Name = "TextBox24"
+        Me.TextBox24.Size = New System.Drawing.Size(75, 75)
+        Me.TextBox24.TabIndex = 23
+        '
+        'TextBox25
+        '
+        Me.TextBox25.BackColor = System.Drawing.Color.White
+        Me.TextBox25.Enabled = False
+        Me.TextBox25.Location = New System.Drawing.Point(327, 327)
+        Me.TextBox25.Multiline = True
+        Me.TextBox25.Name = "TextBox25"
+        Me.TextBox25.Size = New System.Drawing.Size(75, 75)
+        Me.TextBox25.TabIndex = 24
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.MediumSeaGreen
+        Me.ClientSize = New System.Drawing.Size(406, 405)
+        Me.Controls.Add(Me.FlowLayoutPanel1)
+        Me.Name = "Form1"
+        Me.Text = "5x5 Seven Segment"
+        Me.FlowLayoutPanel1.ResumeLayout(False)
+        Me.FlowLayoutPanel1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents FlowLayoutPanel1 As FlowLayoutPanel
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents TextBox6 As TextBox
+    Friend WithEvents TextBox7 As TextBox
+    Friend WithEvents TextBox8 As TextBox
+    Friend WithEvents TextBox9 As TextBox
+    Friend WithEvents TextBox10 As TextBox
+    Friend WithEvents TextBox11 As TextBox
+    Friend WithEvents TextBox12 As TextBox
+    Friend WithEvents TextBox13 As TextBox
+    Friend WithEvents TextBox14 As TextBox
+    Friend WithEvents TextBox15 As TextBox
+    Friend WithEvents TextBox16 As TextBox
+    Friend WithEvents TextBox17 As TextBox
+    Friend WithEvents TextBox18 As TextBox
+    Friend WithEvents TextBox19 As TextBox
+    Friend WithEvents TextBox20 As TextBox
+    Friend WithEvents TextBox21 As TextBox
+    Friend WithEvents TextBox22 As TextBox
+    Friend WithEvents TextBox23 As TextBox
+    Friend WithEvents TextBox24 As TextBox
+    Friend WithEvents TextBox25 As TextBox
+End Class
---
+++ SevenSegment/SevenSegment/Form1.vb
@@ -0,0 +1,167 @@
+﻿Public Class Form1
+    Dim x As Integer = 0
+
+    Private Sub wait(ByVal interval As Integer)
+        Dim sw As New Stopwatch
+        sw.Start()
+        Do While sw.ElapsedMilliseconds < interval
+            ' Allows UI to remain responsive
+            Application.DoEvents()
+        Loop
+        sw.Stop()
+    End Sub
+
+    Private Sub Zero()
+        TextBox1.BackColor = Color.Turquoise
+        TextBox2.BackColor = Color.Turquoise
+        TextBox3.BackColor = Color.Turquoise
+        TextBox4.BackColor = Color.Turquoise
+        TextBox5.BackColor = Color.Turquoise
+        TextBox6.BackColor = Color.Turquoise
+        TextBox10.BackColor = Color.Turquoise
+        TextBox11.BackColor = Color.Turquoise
+        TextBox12.BackColor = Color.White
+        TextBox13.BackColor = Color.White
+        TextBox14.BackColor = Color.White
+        TextBox15.BackColor = Color.Turquoise
+        TextBox16.BackColor = Color.Turquoise
+        TextBox20.BackColor = Color.Turquoise
+        TextBox21.BackColor = Color.Turquoise
+        TextBox22.BackColor = Color.Turquoise
+        TextBox23.BackColor = Color.Turquoise
+        TextBox24.BackColor = Color.Turquoise
+        TextBox25.BackColor = Color.Turquoise
+        x += 1
+    End Sub
+
+
+    Private Sub One()
+        TextBox1.BackColor = Color.White
+        TextBox2.BackColor = Color.White
+        TextBox3.BackColor = Color.White
+        TextBox4.BackColor = Color.White
+        TextBox5.BackColor = Color.Turquoise
+        TextBox6.BackColor = Color.White
+        TextBox10.BackColor = Color.Turquoise
+        TextBox11.BackColor = Color.White
+        TextBox12.BackColor = Color.White
+        TextBox13.BackColor = Color.White
+        TextBox14.BackColor = Color.White
+        TextBox15.BackColor = Color.Turquoise
+        TextBox16.BackColor = Color.White
+        TextBox20.BackColor = Color.Turquoise
+        TextBox21.BackColor = Color.White
+        TextBox22.BackColor = Color.White
+        TextBox23.BackColor = Color.White
+        TextBox24.BackColor = Color.White
+        TextBox25.BackColor = Color.Turquoise
+        x += 1
+    End Sub
+    Private Sub Two()
+        TextBox1.BackColor = Color.Turquoise
+        TextBox2.BackColor = Color.Turquoise
+        TextBox3.BackColor = Color.Turquoise
+        TextBox4.BackColor = Color.Turquoise
+        TextBox11.BackColor = Color.Turquoise
+        TextBox12.BackColor = Color.Turquoise
+        TextBox13.BackColor = Color.Turquoise
+        TextBox14.BackColor = Color.Turquoise
+        TextBox16.BackColor = Color.Turquoise
+        TextBox20.BackColor = Color.White
+        TextBox21.BackColor = Color.Turquoise
+        TextBox22.BackColor = Color.Turquoise
+        TextBox23.BackColor = Color.Turquoise
+        TextBox24.BackColor = Color.Turquoise
+        x += 1
+    End Sub
+
+    Private Sub Three()
+        TextBox16.BackColor = Color.White
+        TextBox20.BackColor = Color.Turquoise
+        x += 1
+    End Sub
+
+    Private Sub Four()
+        TextBox2.BackColor = Color.White
+        TextBox3.BackColor = Color.White
+        TextBox4.BackColor = Color.White
+        TextBox6.BackColor = Color.Turquoise
+        TextBox21.BackColor = Color.White
+        TextBox22.BackColor = Color.White
+        TextBox23.BackColor = Color.White
+        TextBox24.BackColor = Color.White
+        x += 1
+    End Sub
+    Private Sub Five()
+        TextBox2.BackColor = Color.Turquoise
+        TextBox3.BackColor = Color.Turquoise
+        TextBox4.BackColor = Color.Turquoise
+        TextBox10.BackColor = Color.White
+        TextBox20.BackColor = Color.Turquoise
+        TextBox21.BackColor = Color.Turquoise
+        TextBox22.BackColor = Color.Turquoise
+        TextBox23.BackColor = Color.Turquoise
+        TextBox24.BackColor = Color.Turquoise
+        x += 1
+    End Sub
+    Private Sub Six()
+        TextBox16.BackColor = Color.Turquoise
+        x += 1
+    End Sub
+    Private Sub Seven()
+        TextBox6.BackColor = Color.White
+        TextBox10.BackColor = Color.Turquoise
+        TextBox11.BackColor = Color.White
+        TextBox12.BackColor = Color.White
+        TextBox13.BackColor = Color.White
+        TextBox14.BackColor = Color.White
+        TextBox16.BackColor = Color.White
+        TextBox21.BackColor = Color.White
+        TextBox22.BackColor = Color.White
+        TextBox23.BackColor = Color.White
+        TextBox24.BackColor = Color.White
+        x += 1
+    End Sub
+    Private Sub Eight()
+        TextBox6.BackColor = Color.Turquoise
+        TextBox11.BackColor = Color.Turquoise
+        TextBox12.BackColor = Color.Turquoise
+        TextBox13.BackColor = Color.Turquoise
+        TextBox14.BackColor = Color.Turquoise
+        TextBox16.BackColor = Color.Turquoise
+        TextBox20.BackColor = Color.Turquoise
+        TextBox21.BackColor = Color.Turquoise
+        TextBox22.BackColor = Color.Turquoise
+        TextBox23.BackColor = Color.Turquoise
+        TextBox24.BackColor = Color.Turquoise
+        x += 1
+    End Sub
+    Private Sub Nine()
+        TextBox16.BackColor = Color.White
+        x = 0
+    End Sub
+    Private Sub FlowLayoutPanel1_Paint(sender As Object, e As PaintEventArgs) Handles FlowLayoutPanel1.Paint
+        While x < 10
+            Zero()
+            wait(1000)
+            One()
+            wait(1000)
+            Two()
+            wait(1000)
+            Three()
+            wait(1000)
+            Four()
+            wait(1000)
+            Five()
+            wait(1000)
+            Six()
+            wait(1000)
+            Seven()
+            wait(1000)
+            Eight()
+            wait(1000)
+            Nine()
+            wait(1000)
+        End While
+    End Sub
+End Class
